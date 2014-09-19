28e6d72580002b92c373b467e7b242551e283b92
Evert Harmeling <evert.harmeling@freshheads.com>
2014-09-19 23:46:55 +0200

    Command added

--- src/Brouwkuyp/Bundle/DashboardBundle/Command/BrewCommand.php
+++ src/Brouwkuyp/Bundle/DashboardBundle/Command/BrewCommand.php
@@ -2,7 +2,24 @@
 
 namespace Brouwkuyp\Bundle\DashboardBundle\Command;
 
-class BrewCommand
-{
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
 
+class BrewCommand extends Command
+{
+    protected function configure()
+    {
+        $this
+            ->setName('brouwkuyp:brew')
+            ->setDescription('Start the brewery of beer!')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('<info>We gaan starten met brouwen!</info>');
+    }
 }
