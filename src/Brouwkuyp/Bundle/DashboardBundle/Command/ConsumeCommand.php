c07be7d6e4491f0fb7d1cb3df651f861996e81b5
Evert Harmeling <evert.harmeling@freshheads.com>
2014-09-23 23:11:06 +0200

    Trying to get publish / subscribe to work

--- src/Brouwkuyp/Bundle/DashboardBundle/Command/ConsumeCommand.php
+++ src/Brouwkuyp/Bundle/DashboardBundle/Command/ConsumeCommand.php
@@ -8,15 +8,13 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class BrewCommand extends ContainerAwareCommand
+class ConsumeCommand extends ContainerAwareCommand
 {
-    const SET_TEMP = 'brewery/brewhouse01/masher/set_temp';
-
     protected function configure()
     {
         $this
-            ->setName('brouwkuyp:brew')
-            ->setDescription('Start the brewery of beer!')
+            ->setName('brouwkuyp:consume')
+            ->setDescription('Consuming the data')
         ;
     }
 
@@ -24,9 +22,6 @@ class BrewCommand extends ContainerAwareCommand
     {
         $output->writeln('<info>We gaan starten met brouwen!</info>');
 
-        $producer = $this->getContainer()->get('old_sound_rabbit_mq.brouwkuyp_producer');
-        $setTemp = 62;
-        $producer->publish($setTemp, static::SET_TEMP);
-        $output->writeln(sprintf("<info>%s:%s</info>", static::SET_TEMP, $setTemp));
+
     }
 }
