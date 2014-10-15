f974defb8751909ddc2c3499a2776430c9e8a3a8
Luuk van Hal <lvanhal@gmail.com>
2014-10-15 21:40:17 +0200

    brewcommand update

--- src/Brouwkuyp/Bundle/DashboardBundle/Command/BrewCommand.php
+++ src/Brouwkuyp/Bundle/DashboardBundle/Command/BrewCommand.php
@@ -13,33 +13,20 @@ use Symfony\Component\Console\Output\OutputInterface;
  *
  * @author Evert Harmeling <evert.harmeling@freshheads.com>
  */
-class BrewCommand extends ContainerAwareCommand
-{
-    const SET_TEMP = 'brewery.brewhouse01.masher.set_temp';
-
-    protected function configure()
+class BrewCommand extends ContainerAwareCommand {
+	const SET_TEMP = 'brewery.brewhouse01.masher.set_temp';
+	protected function configure() {
+		$this->setName ( 'brouwkuyp:brew' )->setDescription ( 'Start the brewery of beer!' );
+	}
+	protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $this
-            ->setName('brouwkuyp:brew')
-            ->setDescription('Start the brewery of beer!')
-        ;
-    }
-
-    protected function execute(InputInterface $input, OutputInterface $output)
-    {
-        $output->writeln('<info>We are gonna send a message!</info>');
-        $setTemp = 62;
-
-        $connection = new AMQPConnection('localhost', 5672, 'guest', 'guest');
-        $channel = $connection->channel();
-        $channel->exchange_declare('brouwkuyp', 'topic', false, false, false);
-
-        $msg = new AMQPMessage($setTemp);
-        $channel->basic_publish($msg, 'brouwkuyp', static::SET_TEMP);
-
-        $output->writeln(sprintf("<info>Message sent: </info> %s : %s", static::SET_TEMP, $setTemp));
-
-        $channel->close();
-        $connection->close();
+        $output->writeln('<info>Start while</info>');
+        $loopCount = 100;
+        while($loopCount > 0){
+        	usleep(100000);
+        	$output->writeln(sprintf('<info>Loop: %s</info>', $loopCount));
+        	$loopCount--;
+        }
+        
     }
 }
