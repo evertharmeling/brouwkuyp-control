cb00acde9b21e227b13a787862c34b4c79b65bc3
Evert Harmeling <evert.harmeling@freshheads.com>
2014-10-18 14:01:25 +0200

    Major refactoring

--- src/Brouwkuyp/Bundle/LogicBundle/Command/BrewCommand.php
+++ src/Brouwkuyp/Bundle/LogicBundle/Command/BrewCommand.php
@@ -14,7 +14,7 @@ use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-/**
+/** 
  * BrewCommand
  *
  * @author Evert Harmeling <evertharmeling@gmail.com>
