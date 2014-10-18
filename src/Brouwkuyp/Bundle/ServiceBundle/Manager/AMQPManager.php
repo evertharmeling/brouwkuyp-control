cdba4dbd840a5a811207f603ae05dab505a02ebf
Evert Harmeling <evert.harmeling@freshheads.com>
2014-10-18 19:03:21 +0200

    Configured PHPUnit + Refactored ConsumeCommand

--- src/Brouwkuyp/Bundle/ServiceBundle/Manager/AMQPManager.php
+++ src/Brouwkuyp/Bundle/ServiceBundle/Manager/AMQPManager.php
@@ -8,6 +8,10 @@ use PhpAmqpLib\Message\AMQPMessage;
 
 class AMQPManager
 {
+    const MESSAGE_TYPE  = 'topic';
+    const CHANNEL_NAME  = 'amq.topic';
+    const TOPIC_NAME    = 'brouwkuyp';
+
     /**
      * @var AMQPStreamConnection
      */
@@ -18,22 +22,62 @@ class AMQPManager
      */
     protected $channel;
 
+    /**
+     * @var string
+     */
+    protected $queueName;
+
     public function __construct(AMQPStreamConnection $conn)
     {
         $this->conn = $conn;
         $this->channel = $conn->channel();
 
-        list($queueName) = $this->channel->queue_declare('', false, true, true, false);
+        list($this->queueName) = $this->channel->queue_declare('', false, true, true, false);
 
-        //$this->channel->queue_bind($queueName, 'amq.topic', 'brewery.#');
-        $this->channel->exchange_declare('', 'amp.topic', false, true, true, false);
+        $this->channel->exchange_declare(self::TOPIC_NAME, self::MESSAGE_TYPE, false, true, true, false);
     }
 
     /**
      * @param AMQPMessage $message
+     * @param string $routingKey
      */
-    public function publish(AMQPMessage $message)
+    public function publish(AMQPMessage $message, $routingKey)
     {
-        $this->channel->basic_publish($message);
+        $this->channel->basic_publish($message, self::CHANNEL_NAME, $routingKey);
+    }
+
+    /**
+     * @param $callback
+     * @param string $routingKey
+     */
+    public function consume($callback, $routingKey = 'brewery.#')
+    {
+        $this->channel->queue_bind($this->queueName, self::CHANNEL_NAME, $routingKey);
+        $this->channel->basic_consume($this->queueName, '', false, true, false, false, $callback);
+    }
+
+    /**
+     * @return array
+     */
+    public function receive()
+    {
+        return $this->channel->callbacks;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function wait()
+    {
+        return $this->channel->wait();
+    }
+
+    /**
+     * Closes the channel and connection
+     */
+    public function close()
+    {
+        $this->channel->close();
+        $this->conn->close();
     }
 }
