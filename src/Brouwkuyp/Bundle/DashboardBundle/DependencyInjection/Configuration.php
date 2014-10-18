f53aef508062aca8c5adfe2642feb0651cd6a873
Evert Harmeling <evert.harmeling@freshheads.com>
2014-10-18 13:37:36 +0200

    Added DashboardBundle

--- src/Brouwkuyp/Bundle/DashboardBundle/DependencyInjection/Configuration.php
+++ src/Brouwkuyp/Bundle/DashboardBundle/DependencyInjection/Configuration.php
@@ -1,10 +1,6 @@
 <?php
 
-<<<<<<< HEAD:src/Brouwkuyp/Bundle/ServiceBundle/DependencyInjection/Configuration.php
-namespace Brouwkuyp\Bundle\ServiceBundle\DependencyInjection;
-=======
-namespace Brouwkuyp\Bundle\BrewBundle\DependencyInjection;
->>>>>>> Renamed bundle namespaces:src/Brouwkuyp/Bundle/BrewBundle/DependencyInjection/Configuration.php
+namespace Brouwkuyp\Bundle\DashboardBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
@@ -22,11 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-<<<<<<< HEAD:src/Brouwkuyp/Bundle/ServiceBundle/DependencyInjection/Configuration.php
-        $rootNode = $treeBuilder->root('brouwkuyp_service');
-=======
-        $rootNode = $treeBuilder->root('brouwkuyp_brew');
->>>>>>> Added DashboardBundle:src/Brouwkuyp/Bundle/BrewBundle/DependencyInjection/Configuration.php
+        $rootNode = $treeBuilder->root('brouwkuyp_dashboard');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
