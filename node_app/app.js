77642013411a5a3c8ba0e580fc3c44e134ae843a
Evert Harmeling <evert.harmeling@freshheads.com>
2014-10-26 17:31:37 +0100

    node amqp module

--- node_app/app.js
+++ node_app/app.js
@@ -1,19 +1,48 @@
+// load modules
+var nconf = require('nconf');
+nconf.file('settings.json');
+var debug = nconf.get('debug');
 var amqp = require('amqp');
+var _ = require('underscore');
+var util = require("util");
 
-var conn = amqp.createConnection({ url: "amqp://guest:guest@localhost:5672" });
+// intialize connection with AMQP server
+var conn = amqp.createConnection({
+        host: nconf.get('amqp:host'),
+        port: nconf.get('amqp:port'),
+        login: nconf.get('amqp:user'),
+        password: nconf.get('amqp:pass')
+    }, {
+        defaultExchangeName : nconf.get('amqp:exchangeTopic')
+    }
+);
 
-console.log('Listening to queue messages...');
-// Wait for connection to become established.
+// on connect
+conn.on('connect', function () {
+    console.log('Connected to AMQP server');
+    console.log('Listening to AMQP messages...');
+});
+
+// on ready
 conn.on('ready', function () {
     // Use the default 'amq.topic' exchange
-    conn.queue('my-queue', function (q) {
+    conn.queue('my-queue', { durable: true }, function (q) {
         // Catch all messages
-        q.bind('#');
+        q.bind(nconf.get('amqp:routingKey'));
 
         // Receive messages
-        q.subscribe(function (message) {
-            // Print messages to stdout
-            console.log(message);
+        q.subscribe(function (message, headers, deliveryInfo, messageObject) {
+            console.log('Node AMQP: received topic "' + deliveryInfo.routingKey + '", message: "' + message.data.toString() + '"');
         });
     });
+});
+
+// on error
+conn.on('error', function (err) {
+    console.error(util.inspect(err, {depth: null}));
+});
+
+// on close
+conn.on('close', function () {
+    console.log('AMQP connection closed...');
 });
